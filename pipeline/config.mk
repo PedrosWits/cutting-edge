751301ec0a3b56ef37be8ad76b89c6cf2f999ca4
Pedro Pinto da Silva (PGR) <ppintodasilva@gmail.com>
2016-11-22 17:51:55 +0000

    Completed makefile and tested with dummy files. Working version but not optimized.

--- pipeline/config.mk
+++ pipeline/config.mk
@@ -1,10 +1,12 @@
 # Files created and used throughout the pipeline
 RAW_CSV = dataset/raw.csv
-CATEGORISED_CSV = categorised/categorised.csv
+CATEGORISED_CSV = categorise/categorised.csv
 EXTRACTED_CSV = extract/extracted.csv
 ADDITIONAL_CSV = additional/toadd.csv
 MERGED_CSV = merge/merged.csv
 UNIQUE_CSV = unique/unique.csv
+QUERY_RESULTS = queryDB/query_results.txt
+PARSED_IMAGE_LINKS = linking/parsedLinks.txt
 
 # 0. Download Dataset 
 GET_DATASET_SRC = dataset/dl_dataset.sh
@@ -29,3 +31,18 @@ MERGE_EXE = bash $(MERGE_SRC) $(EXTRACTED_CSV) $(ADDITIONAL_CSV) $(MERGED_CSV)
 # 5. Remove duplicates
 UNIQUE_SRC = unique/removeDuplicates.sh
 UNIQUE_EXE = bash $(UNIQUE_SRC) $(MERGED_CSV) $(UNIQUE_CSV)
+
+# 6. Building the database
+INSERT_DATABASE_SRC = insertDB/createDB.py
+INSERT_DATABASE_EXE = python $(INSERT_DATABASE_SRC) $(UNIQUE_CSV)
+
+# 7. Querying the database
+QUERY_DATABASE_SRC = queryDB/queries.py
+QUERY_DATABASE_EXE = python $(QUERY_DATABASE_SRC) $(QUERY_RESULTS)
+
+# 8. Linking images to database
+PARSE_APACHE_SRC = linking/parseApache.sh
+PARSE_APACHE_EXE = bash $(PARSE_APACHE_SRC) $(PARSED_IMAGE_LINKS)
+
+UPDATE_DATABASE_SRC = linking/updateDB.sh
+UPDATE_DATABASE_EXE = python $(UPDATE_DATABASE_SRC) $(PARSED_IMAGE_LINKS)

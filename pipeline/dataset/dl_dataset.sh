22b7e60af63b5cc3b25d41101d59aacb0081bc8d
Pedro Pinto da Silva (PGR) <ppintodasilva@gmail.com>
2016-11-22 14:13:16 +0000

    First make test for download dataset

---
+++ pipeline/dataset/dl_dataset.sh
@@ -0,0 +1,1 @@
+curl -o $1 http://homepages.cs.ncl.ac.uk/matthew.forshaw/teaching/csc8622/cswk/data/cuttingedge.csv
--- travis_test.py
+++
@@ -1,6 +0,0 @@
-def square(x):
-    return x * x
-
-def test_square():
-    assert square(2) == 4
-    assert square(3) == 9

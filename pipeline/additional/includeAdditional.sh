74731718d37ca945b26ad2f414b46825876f24dc
Pedro Pinto da Silva (PGR) <ppintodasilva@gmail.com>
2016-11-23 02:46:40 +0000

    Implemented task 3 and fixed bugs

--- pipeline/additional/includeAdditional.sh
+++ pipeline/additional/includeAdditional.sh
@@ -1,4 +1,23 @@
-for i in *.txt
-do
-	cat  $i | awk -F "=" 'BEGIN{ORS=","}; {print $2};{print "\n"}' 
+# Get TXT files
+zipfile="txtfiles.zip"
+curl -s -o $zipfile http://homepages.cs.ncl.ac.uk/matthew.forshaw/teaching/csc8622/cswk/data/cuttingedge.zip
+
+txtdir="txt"
+mkdir -p $txtdir
+unzip -o -q $zipfile -d $txtdir
+
+rm $zipfile
+
+# Create CSV
+outfile=$1
+echo "Accession identifier, Simple name, Category, Material, Provenance" > $outfile
+
+for filename in $(ls -p $txtdir | grep -v /); do
+    while read -r line || [ -n "$line" ]; do
+        word=$(echo "$line" | cut -d '=' -f 2)
+        printf '\"%q\",' "$word" >> "$outfile"
+    done <$(PWD)/$txtdir/$filename
+    echo >> $outfile
 done
+
+cat $outfile | tr -d $ | sed -e 's/\\r//g' | tr -d \\ | tr -d \' | sed s'/,$//' > $outfile

096101ddbe4e192f6446537b666969f23d6ffa12
Pedro Pinto da Silva (PGR) <ppintodasilva@gmail.com>
2016-11-23 16:16:51 +0000

    Removed trailing space

--- pipeline/merge/mergeFiles.sh
+++ pipeline/merge/mergeFiles.sh
@@ -1,4 +1,4 @@
-echo "Accession identifer,Simple name,Category, Material,Provenance" > $1
+echo "Accession identifer,Simple name,Category,Material,Provenance" > $1
 
 for file in "${@:2}"; do
     sed '1d' $file >> $1    
